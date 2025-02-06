e9a56cb3f604cfef922ec129ad7cfa7042e27d8b
DanielAmaralH <danieldoamaralhillesheim@gmail.com>
2025-02-06 20:25:57 -0300

    Desafio completo

---
+++ index.js
@@ -0,0 +1,25 @@
+// alert("Hello World")
+
+// let num1 = 16;
+
+// if (num1 == 8, 9, 10) {
+//     console.log("Você está na turma infantil");
+// }
+// else if (num1 == 11, 12, 13) {
+//     console.log("Você está na turma juvenil");
+// }
+// else if(num1 == 14, 15, 16){
+//     console.log("Você está na turma jovem adulto");
+// }
+
+// let num1 = 16;
+
+if ([8, 9, 10].includes(num1)) {
+    console.log("Você está na turma infantil");
+}
+else if ([11, 12, 13].includes(num1)) {
+    console.log("Você está na turma juvenil");
+}
+else if ([14, 15, 16].includes(num1)) {
+    console.log("Você está na turma jovem adulto");
+}
